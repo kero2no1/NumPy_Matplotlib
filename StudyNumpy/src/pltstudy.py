2380ad96a93fac59913193fe2d4a1b3d3c0f27f3
kero2no1 <kerokero.2no1@gmail.com>
2023-06-03 23:16:27 +0900

    棒グラフやグラフの詳細設定

--- StudyNumpy/src/pltstudy.py
+++ StudyNumpy/src/pltstudy.py
@@ -9,41 +9,65 @@ import numpy as np
 
 
 def exercise1():
+    # 折れ線グラフ
     x = np.arange(10)
     y = np.random.randint(-10,10,10)
     
-    # 折れ線グラフ
     # plt.plot(y)     # x軸を指定しない場合はyのインデックスを横軸にとる
     plt.plot(x,y)
+
+    # グラフの詳細設定
+    plt.title("Result")     # タイトル
+    plt.xlabel("x axis")    # x軸ラベル
+    plt.ylabel("y axis")    # y軸ラベル
 
     plt.show()
 
 def exercise2():
+    # 散布図
     x1 = np.random.randint(10,20,20)
     x2 = np.random.randint(20,30,20)
     y1 = np.random.randint(50,100,20)
     y2 = np.random.randint(0,40,20)
     
-    # 散布図
     plt.scatter(x1,y1)
     plt.scatter(x2,y2)
+
+    # グラフの詳細設定
+    plt.xlim(0,40)
+    plt.ylim(0,120)
+
 
     plt.show()
 
 def exercise3():
+    # ヒストグラム
     data = np.random.randint(0,10,10)
     
-    # ヒストグラム
     ret = plt.hist(data) # retに度数と階級値を取得できる
     print( ret )
 
     plt.show()
 
 def exercise4():
-    pass
+    # 棒グラフ
+    x = ["Sam","John","Kevin","Adam"]
+    y = np.random.randint(0,200,4)
+    
+    plt.bar( x, y )
+    
+    plt.show()
+    
 
 def exercise5():
-    pass
+    # 対数軸
+    x = np.linspace(0,10,500)   # 0～10 を 500分割
+    y = np.exp(x)
+    
+    plt.plot(x,y)
+    plt.yscale("log")           # Y軸を対数軸に
+    
+    plt.show()
 
 def exercise6():
     pass
@@ -51,9 +75,9 @@ def exercise6():
 def main():
     # exercise1()
     # exercise2()
-    exercise3()
+    # exercise3()
     # exercise4()
-    # exercise5()
+    exercise5()
     # exercise6()
     # exercise7()
 
