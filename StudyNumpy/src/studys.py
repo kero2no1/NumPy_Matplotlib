6ffd6f64f8affc2d521b785214eed069b02cbf44
kero2no1 <kerokero.2no1@gmail.com>
2023-06-05 22:23:57 +0900

    NumPy、ユニバーサル関数と行列の結合・分解

--- StudyNumpy/src/studys.py
+++ StudyNumpy/src/studys.py
@@ -94,7 +94,7 @@ def exercise2():
     arr_1 = np.array([[2,3,4]])     # 1次元ではなく2次元になるように定義する
     arr_2 = np.array([[1.2,3.5,5.1],[-0.3,1.1,-4.5]])
     print( arr_1 )
-    print( arr_1.T )    # ベクトルの転置はndarrayの作り方に注意
+    print( arr_1.T )                # ベクトルの転置はndarrayの作り方に注意
     arr_1 = np.array([2,3,4])
     print( arr_1.reshape(-1,1) )    # 無理やりreshape
     print( arr_2 )
@@ -125,21 +125,62 @@ def exercise3():
     
     # 統計値
     arr_1 = np.array([[2,4,6],[-1,5,-3],[0,-2,3]])
-    print( arr_1.max() )    # 各要素の最大(引数にaxis=を追加すると、列ごと行ごとの統計値を算出可能)
-    print( arr_1.min() )    # 各要素の最小
-    print( arr_1.sum() )    # 各要素の総和
-    print( arr_1.mean() )   # 各要素の平均
-    print( arr_1.var() )    # 各要素の分散
-    print( arr_1.std() )    # 各要素の標準偏差
+    print( arr_1.max() )            # 各要素の最大(引数にaxis=を追加すると、列ごと行ごとの統計値を算出可能)
+    print( arr_1.min() )            # 各要素の最小
+    print( arr_1.sum() )            # 各要素の総和
+    print( arr_1.mean() )           # 各要素の平均
+    print( arr_1.var() )            # 各要素の分散
+    print( arr_1.std() )            # 各要素の標準偏差
     
     
+
+def exercise4():
+    # ユニバーサル関数 ・・・ndarrayの全要素の要素ごとに演算処理するもの
+    arr_1 = np.array([0,1,2,4]).reshape(2,-1)
+    print( arr_1 )
+    print( np.sqrt( arr_1 ) )       # 平方根
+    print( np.exp( arr_1 ) )        # exp
+    
+    arr_1 = np.array([0, np.pi, np.pi/2, -np.pi/4]).reshape(2,-1)
+    print( arr_1 )
+    print( np.sin( arr_1 ) )
+    print( np.cos( arr_1 ) )
+
+def exercise5():
+    # 行列の結合と分解
+    arr_1 = np.array([0,1,-1,2,4,-3,5,-2,7]).reshape(3,-1)
+    arr_2 = np.array([-2,0,1,5,-1,2,-6,3,4]).reshape(3,-1)
+    print( arr_1 )
+    print( arr_2 )
+    
+    arr_3 = np.vstack( (arr_1,arr_2) )  # 縦方向(vertical)結合
+    print( arr_3 )
+    print( np.vsplit( arr_3, 2 ) )      # 縦方向の分解(２つに分解した)
+    print( np.vsplit( arr_3, 2 )[0] )   # ２つのうちの1つ目は配列要素で抽出できる
+    print( np.vsplit( arr_3, 2 )[1] )
+    
+    arr_3 = np.hstack( (arr_1,arr_2) )  # 横方向(horizontal)結合
+    print( arr_3 )
+    print( np.hsplit( arr_3, 2 ) )      # 横方向の分解(２つに分解)
+    print( np.hsplit( arr_3, 2 )[0] )   # ２つのうちの1つ目は配列要素で抽出
+    print( np.hsplit( arr_3, 2 )[1] )
+
+def exercise6():
+    pass
+    
+def exercise7():
+    pass
     
 
 def main():
     
     # exercise1()
     # exercise2()
-    exercise3()
+    # exercise3()
+    # exercise4()
+    exercise5()
+    # exercise6()
+    # exercise7()
     
     
 
