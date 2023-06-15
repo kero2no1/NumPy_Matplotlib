a17c7b4712764a57abc30f2118c20113a4503ffa
kero2no1 <kerokero.2no1@gmail.com>
2023-06-15 22:16:31 +0900

    データセットとpandas動作確認

--- StudyNumpy/src/skiris.py
+++ StudyNumpy/src/skiris.py
@@ -9,7 +9,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-def main():
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from glob import glob
+
+def iris_test():
     iris = load_iris()
     X = iris.data[:,[0,2]]
     y = iris.target
@@ -22,6 +27,21 @@ def main():
     pred = model.predict(X_test)
     accuracy = accuracy_score(y_test,pred)
     print(accuracy)
+
+def data_load_pandas():
+    df = pd.read_csv("../dataset/data.csv")
+    print(df.head())
+    print(df.tail())
+    
+    print(df.shape)
+    print(df.isnull().sum())    # 欠損値の確認
+    df.dropna(inplace=True)     # 欠損値除去
+    print(df.shape)
+    print(df.isnull().sum())
+
+def main():
+    # iris_test()
+    data_load_pandas()
     
 
 if __name__ == '__main__':
