b03b6c060d20461651b1b794050a98208cb2b6a9
kero2no1 <kerokero.2no1@gmail.com>
2023-06-16 21:55:11 +0900

    欠損値の補完

--- StudyNumpy/src/skiris.py
+++ StudyNumpy/src/skiris.py
@@ -34,10 +34,42 @@ def data_load_pandas():
     print(df.tail())
     
     print(df.shape)
-    print(df.isnull().sum())    # 欠損値の確認
-    df.dropna(inplace=True)     # 欠損値除去
+    print(df.isnull().sum())        # 欠損値の確認
+    df.dropna(inplace=True)         # 欠損値除去
     print(df.shape)
     print(df.isnull().sum())
+    
+    
+    df = pd.read_csv("../dataset/data.csv")
+    print(df[["Age"]].tail())
+    print(df[["Age"]].mean())       # ある列の平均値を求める
+    print(df[["Age"]].describe())   # 統計値をまとめて取得する
+    
+    df_zero = df[["Age"]].fillna(0) # 欠損値をゼロで補完する
+    print(df_zero[["Age"]].tail())  
+
+    df_mean = df[["Age"]].fillna(int(df["Age"].mean()))     # 欠損値を平均値で補完する
+    print(df_mean[["Age"]].tail())
+    
+    df_median = df[["Age"]].fillna(int(df["Age"].median())) # 欠損値を中央値で補完する
+    print(df_median[["Age"]].tail())
+    
+    # 乱数を使った補完
+    mean = df["Age"].mean()
+    std = df["Age"].std()
+    num = df["Age"].isnull().sum()
+    rn = np.random.randint(mean-std,mean+std,size=num)
+    print(rn)
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2,1,1)
+    ax2 = fig.add_subplot(2,1,2)
+    ax1.hist(df["Age"].dropna().astype(int),bins=70)
+    
+    # df["Age"][df["Age"].isnull()] = rn
+    # print(df["Age"][df["Age"].isnull()])
+    
+    plt.show()
 
 def main():
     # iris_test()
