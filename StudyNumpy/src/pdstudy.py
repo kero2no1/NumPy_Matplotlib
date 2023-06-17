96635cc00ac476c0c8dddd4bfff67bf985740ce0
kero2no1 <kerokero.2no1@gmail.com>
2023-06-17 22:03:55 +0900

    pandasファイルの追加

--- StudyNumpy/src/pdstudy.py
+++ StudyNumpy/src/pdstudy.py
@@ -4,8 +4,20 @@ Created on 2023/06/15
 @author: Keroichi.T
 '''
 
+import pandas as pd
+
+def studyfunc01():
+    df = pd.read_csv("../dataset/weather.csv")
+    print(df.head(3))   # 先頭から引数を指定した行だけ表示
+    print(df.tail(10))  # 末尾から引数を指定した行だけ表示
+
+
 def main():
-    pass    
+    studyfunc01()    
+    # studyfunc02()    
+    # studyfunc03()    
+    # studyfunc04()    
+    # studyfunc05()    
 
 if __name__ == '__main__':
     main()
